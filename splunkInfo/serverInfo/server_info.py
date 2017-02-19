4ba246e817b6b8879bb859e62cc7597a6f710ec7
anabasis <anabasis01@gmail.com>
2017-02-19 15:22:15 +0900

    Init Python Project

---
+++ splunkInfo/serverInfo/server_info.py
@@ -0,0 +1,24 @@
+# This Python file uses the following encoding: utf-8
+
+import requests
+from bs4 import BeautifulSoup
+
+'''
+Created on 2017. 2. 16.
+
+@author: anabasis
+'''
+
+def spider():
+    print("START!!!")
+
+    url = 'https://docs.splunk.com/Documentation/Splunk/6.5.2/Admin/Serverconf'
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "lxml")   ## lxml파서, html.parser파서 비교
+    #print(soup.text)
+    print(soup.contents)     
+    
+    print("END!!!")
+
+spider()    
