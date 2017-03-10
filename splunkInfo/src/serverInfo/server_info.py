ce799c68c5b195e105ee5930315df8cd21b30af2
Cho Jun Seung <anabasis01@gmail.com>
2017-03-10 18:19:33 +0900

    BeautifulSoup

--- splunkInfo/src/serverInfo/server_info.py
+++ splunkInfo/src/serverInfo/server_info.py
@@ -1,6 +1,7 @@
 # This Python file uses the following encoding: utf-8
 
 import requests
+import re
 from bs4 import BeautifulSoup
 
 '''
@@ -17,8 +18,17 @@ def spider():
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")   ## lxml파서, html.parser파서 비교
     #print(soup.text)
-    print(soup.contents)     
+    #print(soup.contents)
+    #soup.find_all("","")
+    fTag = soup.find_all("span", class_="mw-headline")
     
+    for ftag in fTag :
+        print(ftag)
+        preTag = ftag.find_parent(re.compile("^h"))
+        nextTag = preTag.find_next_sibling()
+        print(preTag)
+        print(nextTag)
+        
     print("END!!!")
 
-spider()    
+spider()
