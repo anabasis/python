71570373b417f51d094a7937ee54fe49fb208da1
anabasis01 <anabasis01@gmail.com>
2020-07-10 16:16:20 +0900

    rabbitmq

--- rabbitmq/worker/worker.py
+++ rabbitmq/worker/worker.py
@@ -10,7 +10,6 @@ channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
-
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
