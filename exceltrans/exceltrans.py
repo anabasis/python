351efcab85bdb3ec045b1bcc079c095aa704bf00
anabasis01 <anabasis01@gmail.com>
2020-09-11 17:34:53 +0900

    excel

--- exceltrans/exceltrans.py
+++ exceltrans/exceltrans.py
@@ -1,13 +1,77 @@
 #!/usr/bin/env python
 
+# pip install virtualenv
+# virtualenv exceltrans
+# \exceltrans\Scripts\activate
+# python -m pip install --upgrade pip
 # pip install xlsxwriter
 # pip install openpyxl
+
+# Openpyxl <https://doitnow-man.tistory.com/159>
 
 from openpyxl import load_workbook
 
 def xlsx_read(filename):
-    wb2 = load_workbook(filename)
-    print(wb2.sheetnames)
+
+    wb = load_workbook(filename)
+    sheet_names = wb.sheetnames
+    print(sheet_names)
+
+    for sheet_name in sheet_names:
+        print(sheet_name)
+
+        sheet_xlsx = wb[sheet_name]
+
+        print(sheet_xlsx.max_row)
+        print(sheet_xlsx.max_column)
+
+        for row in range(1, sheet_xlsx.max_row):
+            for col in range(1, sheet_xlsx.max_column):
+                print(sheet_xlsx.cell(row,col).value)
+
+    wb.close()
+    return print(sheet_names)
+
+def xlsx_sheet_read(filename, sheetname='Sheet1'):
+    wb = load_workbook(filename)
+    sheet_xlsx = wb[sheetname]
+
+    print(sheet_xlsx.max_row)
+    print(sheet_xlsx.max_column)
+
+    for row in range(1, sheet_xlsx.max_row):
+        for col in range(1, sheet_xlsx.max_column):
+            print(sheet_xlsx.cell(row,col).value)
+
+    wb.close()
+    return print(sheetname)
+
+def xlsx_sheet_trans(filename, sheetname='Sheet1', key='A', fields=[]):
+    wb = load_workbook(filename)
+    sheet_xlsx = wb[sheetname]
+
+    print('## SIZE  ##########################################################')
+    print(sheet_xlsx.max_row)
+    print(sheet_xlsx.max_column)
+    print('###################################################################')
+
+    for row in range(1, 4):
+
+        if sheet_xlsx.cell(row,col) == None :
+            break
+
+        for col in range(1, sheet_xlsx.max_column):
+            print(sheet_xlsx.cell(row,col).value)
+        print('###################################################################')
+
+    wb.close()
+    return print(sheetname)
+
+    return ''
+
+
+
+
 
 def xlsx_writer(data):
     print(data)
@@ -15,4 +79,5 @@ def xlsx_writer(data):
 
 if __name__ == "__main__":
 
-    xlsx_read('E:\\ATOM_WORK\\python\\exceltrans\\data\\ES_시나리오_한글_20200910.xlsx')
+    #xlsx_read('.\\data\\ES_시나리오_한글_20200910.xlsx')
+    xlsx_sheet_trans('.\\data\\ES_시나리오_한글_20200910.xlsx','ANALYTICS','B',['B','F','G','P'])
